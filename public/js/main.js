16f622fefd602979bfd5264e89eb135d2e7c1b16
George Clausing <60722298+SindriWebsites-George@users.noreply.github.com>
2020-03-24 15:40:54 -0700

    Created a drop down menu for mobile

---
+++ public/js/main.js
@@ -0,0 +1,17 @@
+// Side navigation 
+var open = 0;
+
+function toggleNav() {
+
+    if (open == 0) {
+        $(".sideBar").css("display", "block");
+        open++;
+        // console.log("I am open.", open)
+        return open;
+    } else {
+        open--;
+        // console.log("I am closed.", open)
+        $(".sideBar").css("display", "none");
+        return open;
+    }
+}
